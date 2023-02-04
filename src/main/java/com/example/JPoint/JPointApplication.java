d372990518c2514844551521429163ae97c39d2d
WoldemarK <kovtynov.vladimir@gmail.com>
2023-02-04 19:31:37 +0300

    create CommandLineRunner

--- src/main/java/com/example/JPoint/JPointApplication.java
+++ src/main/java/com/example/JPoint/JPointApplication.java
@@ -2,9 +2,11 @@ package com.example.JPoint;
 
 import com.example.JPoint.model.Department;
 import com.example.JPoint.model.Post;
+import com.example.JPoint.model.Task;
 import com.example.JPoint.model.User;
 import com.example.JPoint.repository.DepartmentRepository;
 import com.example.JPoint.repository.PostRepository;
+import com.example.JPoint.repository.TaskRepository;
 import com.example.JPoint.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,58 +19,71 @@ public class JPointApplication {
         SpringApplication.run(JPointApplication.class, args);
     }
 
+//    @Bean
+//    public CommandLineRunner addUser(UserRepository userRepository) {
+//        return args -> {
+//            userRepository.save(User.builder()
+//                    .login("123")
+//                    .password("password")
+//                    .firstName("Ivan")
+//                    .lastName("Tomsin")
+//                    .birth("20.02.1987")
+//                    .phoneNumber("88000000")
+//                    .email("ivan@email.com")
+//                    .isActive(true)
+//                    .sex("M")
+//                    .build());
+//            userRepository.save(User.builder()
+//                    .login("321")
+//                    .password("password321")
+//                    .firstName("Tom")
+//                    .lastName("Mik")
+//                    .birth("12.02.1990")
+//                    .phoneNumber("88000001")
+//                    .email("tom@email.com")
+//                    .isActive(true)
+//                    .sex("M")
+//                    .build());
+//
+//        };
+//    }
+//
+//    @Bean
+//    public CommandLineRunner addPost(PostRepository postRepository) {
+//        return args -> {
+//            postRepository.save(Post.builder()
+//                    .name("Java Developer")
+//                    .description("Разработчик")
+//                    .build());
+//            postRepository.save(Post.builder()
+//                    .name("manager")
+//                    .description("Отдел продаж")
+//                    .build());
+//        };
+//    }
+//    @Bean
+//    public CommandLineRunner addDepartment(DepartmentRepository departmentRepository) {
+//        return args -> {
+//            departmentRepository.save(Department.builder()
+//                    .name("IT")
+//                    .description("Подразделение разработки")
+//                    .build());
+//            departmentRepository.save(Department.builder()
+//                    .name("Продажи")
+//                    .description("Проектные продажи")
+//                    .build());
+//        };
+//    }
     @Bean
-    public CommandLineRunner addUser(UserRepository userRepository) {
+    public CommandLineRunner addTask(TaskRepository taskRepository) {
         return args -> {
-            userRepository.save(User.builder()
-                    .login("123")
-                    .password("password")
-                    .firstName("Ivan")
-                    .lastName("Tomsin")
-                    .birth("20.02.1987")
-                    .phoneNumber("88000000")
-                    .email("ivan@email.com")
-                    .isActive(true)
-                    .sex("M")
+            taskRepository.save(Task.builder()
+                    .name("Фича")
+                    .descriptions("Написать новый функционал")
                     .build());
-            userRepository.save(User.builder()
-                    .login("321")
-                    .password("password321")
-                    .firstName("Tom")
-                    .lastName("Mik")
-                    .birth("12.02.1990")
-                    .phoneNumber("88000001")
-                    .email("tom@email.com")
-                    .isActive(true)
-                    .sex("M")
-                    .build());
-
-        };
-    }
-
-    @Bean
-    public CommandLineRunner addPost(PostRepository postRepository) {
-        return args -> {
-            postRepository.save(Post.builder()
-                    .name("Java Developer")
-                    .description("Разработчик")
-                    .build());
-            postRepository.save(Post.builder()
-                    .name("manager")
-                    .description("Отдел продаж")
-                    .build());
-        };
-    }
-    @Bean
-    public CommandLineRunner addDepartment(DepartmentRepository departmentRepository) {
-        return args -> {
-            departmentRepository.save(Department.builder()
-                    .name("IT")
-                    .description("Подразделение разработки")
-                    .build());
-            departmentRepository.save(Department.builder()
-                    .name("Продажи")
-                    .description("Проектные продажи")
+            taskRepository.save(Task.builder()
+                    .name("Документы")
+                    .descriptions("Подготовить к проектную документацию")
                     .build());
         };
     }
